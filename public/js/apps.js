d2cbd747c01c6ee8f7431de18178c1d0bb585d72
europes <64372763+europes@users.noreply.github.com>
2021-04-23 20:08:03 +0100

    init commit for 2nd time

---
+++ public/js/apps.js
@@ -0,0 +1,30 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    
+messageOne.textContent = 'loading...'
+messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location ).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {            
+            messageOne.textContent = data.error          
+                   }  else {       
+                       console.log(data.forecast)   
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast.Forecast
+        }
+    })
+})
+
+    console.log(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url =   'http://api.weatherstack.com/current?access_key=9c13fc462cfdebade641afaaf0828a5a&query=' + latitude + ',' + longitude 
+        request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find coordinates. Try another search.', undefined)
+        } else {
+            callback( undefined, {
+                Forecast:  body.current.weather_descriptions[0] +'.' + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.'             
+            })
+        }} )
+    }
+
+
+module.exports = forecast
